51659586d09b36076c40f1e5b779a10e32f623c1
titan343 <136104433+titan343@users.noreply.github.com>
2025-07-21 19:56:53 +0800

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -129,6 +129,11 @@ const Sidebar = ({ onToggle, activePath = '/' }: SidebarProps) => {
       href: '/douban?type=movie',
     },
     {
+      icon: Animation,
+      label: '动漫',
+      href: '/douban?type=animation',
+    },
+    {
       icon: Tv,
       label: '剧集',
       href: '/douban?type=tv',
